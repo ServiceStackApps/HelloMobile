2e4fc47132ac99818800db3e96eed63bd7ea4005
Demis Bellot <demis.bellot@gmail.com>
2014-08-09 15:50:52 -0400

    Upgrade to v4.0.30 PCL SS.Interfaces, can now share DTO dll between client/server

--- src/ServiceModel/Properties/AssemblyInfo.cs
+++ src/ServiceModel/Properties/AssemblyInfo.cs
@@ -1,26 +1,20 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ServiceModel")]
+[assembly: AssemblyTitle("PclTest.ServiceModel.Pcl")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ServiceModel")]
+[assembly: AssemblyProduct("PclTest.ServiceModel.Pcl")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("30ad0cd5-fb87-445d-adc7-d993745b15fc")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //

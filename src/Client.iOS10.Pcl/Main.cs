f955a6aa628213a4c374862dd545d059076723bb
Demis Bellot <demis.bellot@gmail.com>
2015-01-15 18:42:41 -0500

    Add Unified API Client.iOS10.Pcl version

--- src/Client.iOS10.Pcl/Main.cs
+++ src/Client.iOS10.Pcl/Main.cs
@@ -1,21 +1,22 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using MonoTouch.Foundation;
-using MonoTouch.UIKit;
 
-namespace Client.iOS
+using Foundation;
+using UIKit;
+
+namespace Client.iOS10.Pcl
 {
 	public class Application
 	{
 		// This is the main entry point of the application.
-		static void Main (string[] args)
+		static void Main(string[] args)
 		{
 			ServiceStack.IosPclExportClient.Configure();
 
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
-			UIApplication.Main (args, null, "AppDelegate");
+			UIApplication.Main(args, null, "AppDelegate");
 		}
 	}
 }

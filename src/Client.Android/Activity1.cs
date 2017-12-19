4342e1da8d30a2ebede8a9a32702741616e03005
Demis Bellot <demis.bellot@gmail.com>
2017-12-19 06:39:20 -0500

    Remove PCL + Upgrade to ServiceStack V5

---
+++ src/Client.Android/Activity1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using ServiceModel;
+using ServiceStack;
+using Shared.Client;
+
+namespace Client.Android
+{
+    [Activity(Label = "Client.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Activity1 : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            //AndroidPclExportClient.Configure();
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var btnSync = FindViewById<Button>(Resource.Id.btnSync);
+            var btnAsync = FindViewById<Button>(Resource.Id.btnAsync);
+            var btnAwait = FindViewById<Button>(Resource.Id.btnAwait);
+            var btnAuth = FindViewById<Button>(Resource.Id.btnAuth);
+            var btnShared = FindViewById<Button>(Resource.Id.btnShared);
+            var txtName = FindViewById<EditText>(Resource.Id.txtName);
+            var lblResults = FindViewById<TextView>(Resource.Id.lblResults);
+
+            //10.0.2.2 = loopback
+            //http://developer.android.com/tools/devices/emulator.html
+            var client = new JsonServiceClient("http://10.0.2.2:2000/");
+            var gateway = new SharedGateway("http://10.0.2.2:2000/");
+
+            btnSync.Click += delegate
+            {
+                try
+                {
+                    var response = client.Get(new Hello { Name = txtName.Text });
+                    lblResults.Text = response.Result;
+
+                    //using (var ms = new MemoryStream("Contents".ToUtf8Bytes()))
+                    //{
+                    //    ms.Position = 0;
+                    //    var fileResponse = client.PostFileWithRequest<HelloResponse>(
+                    //        "/hello", ms, "filename.txt", new Hello { Name = txtName.Text });
+
+                    //    lblResults.Text = fileResponse.Result;
+                    //}
+                }
+                catch (Exception ex)
+                {
+                    lblResults.Text = ex.ToString();
+                }
+            };
+
+            btnAsync.Click += delegate
+            {
+                client.GetAsync(new Hello { Name = txtName.Text })
+                    .Success(response => lblResults.Text = response.Result)
+                    .Error(ex => lblResults.Text = ex.ToString());
+            };
+
+            btnAwait.Click += async delegate
+            {
+                try
+                {
+                    var response = await client.GetAsync(new Hello { Name = txtName.Text });
+                    lblResults.Text = response.Result;
+                }
+                catch (Exception ex)
+                {
+                    lblResults.Text = ex.ToString();
+                }
+            };
+
+            btnAuth.Click += async delegate
+            {
+                try
+                {
+                    await client.PostAsync(new Authenticate
+                    {
+                        provider = "credentials",
+                        UserName = "user",
+                        Password = "pass",
+                    });
+
+                    var response = await client.GetAsync(new HelloAuth { Name = "Secure " + txtName.Text });
+
+                    lblResults.Text = response.Result;
+                }
+                catch (Exception ex)
+                {
+                    lblResults.Text = ex.ToString();
+                }
+            };
+
+            btnShared.Click += async delegate
+            {
+                try
+                {
+                    var greeting = await gateway.SayHello(txtName.Text);
+                    lblResults.Text = greeting;
+                }
+                catch (Exception ex)
+                {
+                    lblResults.Text = ex.ToString();
+                }
+            };
+        }
+    }
+}
+
---
+++ src/Client.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows;
+using ServiceModel;
+using ServiceStack;
+using Shared.Client;
+
+namespace Client.Wpf.Pcl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly JsonServiceClient client;
+        SharedGateway gateway = new SharedGateway();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Net40PclExportClient.Configure();
+            client = new JsonServiceClient("http://localhost:2000/");
+        }
+
+        private void btnSync_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var response = client.Get(new Hello { Name = txtName.Text });
+                lblResults.Text = response.Result;
+            }
+            catch (Exception ex)
+            {
+                lblResults.Text = ex.ToString();
+            }
+        }
+
+        private void btnAsync_Click(object sender, RoutedEventArgs e)
+        {
+            client.GetAsync(new Hello { Name = txtName.Text })
+                .Success(r => lblResults.Text = r.Result)
+                .Error(ex => lblResults.Text = ex.ToString());
+        }
+
+        private async void btnAwait_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var response = await client.GetAsync(new Hello { Name = txtName.Text });
+                lblResults.Text = response.Result;
+            }
+            catch (Exception ex)
+            {
+                lblResults.Text = ex.ToString();
+            }
+        }
+
+        private async void btnAuth_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await client.PostAsync(new Authenticate
+                {
+                    provider = "credentials",
+                    UserName = "user",
+                    Password = "pass",
+                });
+
+                var response = await client.GetAsync(new HelloAuth { Name = "Secure " + txtName.Text });
+
+                lblResults.Text = response.Result;
+            }
+            catch (Exception ex)
+            {
+                lblResults.Text = ex.ToString();
+            }
+        }
+
+        private async void btnGateway_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var greeting = await gateway.SayHello(txtName.Text);
+                lblResults.Text = greeting;
+            }
+            catch (Exception ex)
+            {
+                lblResults.Text = ex.ToString();
+            }
+        }
+    }
+}
